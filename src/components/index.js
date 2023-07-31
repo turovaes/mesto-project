922a1010400443410ac80fa73170d75f95574290
Li7252 <lidiya.yasyrckina@gmail.com>
2023-07-31 18:54:59 +0300

    feat: добавлен класс FormValidator

--- src/components/index.js
+++ src/components/index.js
@@ -3,17 +3,18 @@ import '../pages/index.css';
 import {} from './modal';
 import { createInitialCards } from './card';
 import { loadProfile } from './profile';
-import { enableValidation } from './validate';
+import { FormValidator } from './FormValidator';
 
 loadProfile().then(createInitialCards);
 
-enableValidation({
+const form = new FormValidator({
   formSelector: '.form',
   inputSelector: '.form__input',
   submitButtonSelector: '.form__button',
   inactiveButtonClass: 'form__button_inactive',
   inputErrorClass: 'form__input_error',
   errorClass: 'form__input-error_active'
-});
+})
 
+form.enableValidation();
 
